b2bcf1447dbc16b76270e508223c1d21a664b14c
Agatha Nara <gthlbrc@outlook.com>
2021-09-29 23:41:38 -0300

    a

--- deployBlogPessoal/src/main/java/org/generation/BlogPessoal/controller/UserLoginController.java
+++ deployBlogPessoal/src/main/java/org/generation/BlogPessoal/controller/UserLoginController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/usuario")
+@RequestMapping("/credenciais")
 @CrossOrigin("*")
 public class UserLoginController {
 	
--- deployBlogPessoal/src/main/java/org/generation/BlogPessoal/security/BasicSecurityConfig.java
+++ deployBlogPessoal/src/main/java/org/generation/BlogPessoal/security/BasicSecurityConfig.java
@@ -33,8 +33,8 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 				http.authorizeRequests()
-				.antMatchers("/api/v1/usuario/cadastrar").permitAll()
-				.antMatchers("/api/v1/usuario/logar").permitAll()
+				.antMatchers("/credenciais/cadastrar").permitAll()
+				.antMatchers("/credenciais/logar").permitAll()
 				.antMatchers(HttpMethod.OPTIONS).permitAll()
 				.anyRequest().authenticated().and().httpBasic()
 				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
